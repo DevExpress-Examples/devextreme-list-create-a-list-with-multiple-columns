f77787315f5697f97b30b71ac96488ea252c070e
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-10 20:38:40 +0300

    Implement Angular example

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
-import { DxButtonModule, DxButtonTypes } from 'devextreme-angular/ui/button';
+import { DxListModule } from 'devextreme-angular/ui/list';
+import { tasks } from './data';
 
 @Component({
     selector: 'app-root',
-    imports: [DxButtonModule],
+    imports: [DxListModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
-
-  counter = 0;
-
-  buttonText = 'Click count: 0';
-
-  onClick(_e: DxButtonTypes.ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
-  }
+  tasks = tasks;
 }
