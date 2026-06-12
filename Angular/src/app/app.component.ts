1908b0217398fb1799afaecbf32cd9afbf0300d5
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:26:10 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxListModule } from 'devextreme-angular/ui/list';
 import { tasks } from './data';
 
@@ -6,6 +6,7 @@ import { tasks } from './data';
     selector: 'app-root',
     imports: [DxListModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
